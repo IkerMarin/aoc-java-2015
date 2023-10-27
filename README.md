a1a4d0b02b4cdcd774f699e1b312b593cdd1fdaf
Iker Marin <IkerMarin@users.noreply.github.com>
2023-10-27 20:36:50 +0200

    First changes for exercise one

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # aoc-java-2015
 
 This is an attempt to do all advent of code exercises, just for fun, starting from 2015.
+My idea is to make all daily exercises from scratch, so it's expected to have a lot of replicated code in utils classes.
--- src/main/java/com/github/ikermarin/aoc/year2015/day01/Exercise01.java
+++ src/main/java/com/github/ikermarin/aoc/year2015/day01/Exercise01.java
@@ -1,6 +1,6 @@
-package com.github.ikermarin.aoc.year2015;
+package com.github.ikermarin.aoc.year2015.day01;
 
-public class Day01 {
+public class Exercise01 {
 	public static void main(String args[]) {
 		System.out.println("THIS IS A JUST A TEST");
 	}
